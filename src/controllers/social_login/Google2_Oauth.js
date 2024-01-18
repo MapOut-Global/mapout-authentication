6215133c5fd73f6a406513c34cf12ce0315b19a9
basit-mapout <basit@mapout.com>
2024-01-18 18:41:56 +0530

    added google auth for hrgig

--- src/controllers/social_login/Google2_Oauth.js
+++ src/controllers/social_login/Google2_Oauth.js
@@ -1,27 +1,46 @@
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT);
 const { completeRegistration } = require("../mapout-controllers/otp_login/utils/auth.utils");
 const { completeHRgigRegistration } = require("../hrgig-controllers/utils/auth.utils");
 
 module.exports = {
   googleAuth: async (req, res) => {
     try {
-      const { token, deviceToken, from, organisationName } = await req.body;
+      const { token, deviceToken, requestFrom, organisationName } = req.body;
+      let client
+      let ticket
 
-      const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: [
-          process.env.GOOGLE_ANDROID_CLIENT_ID,
-          process.env.GOOGLE_IOS_CLIENT_ID,
-          process.env.GOOGLE_WEB_CLIENT_ID,
-        ],
-      });
-      
+      switch (requestFrom) {
+        case "app":
+          client = new OAuth2Client(process.env.MAPOUT_GOOGLE_CLIENT);
+          ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: [
+              process.env.GOOGLE_ANDROID_CLIENT_ID,
+              process.env.GOOGLE_IOS_CLIENT_ID,
+              process.env.GOOGLE_WEB_CLIENT_ID,
+            ],
+          });
+          break;
+
+        case "hrgig":
+          client = new OAuth2Client(process.env.HRGIG_GOOGLE_CLIENT);
+          ticket = await client.verifyIdToken({
+            idToken: token
+          });
+          break;
+
+        case "dew":
+          // For dew
+          break;
+
+        default:
+          break;
+      }
       const { email, name, email_verified } = ticket.getPayload();
 
       if (email_verified) {
         let registerUser;
-        switch (from) {
+        switch (requestFrom) {
           case "app":
             registerUser = await completeRegistration({
               email,
